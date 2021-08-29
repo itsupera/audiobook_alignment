bf74f26bb4b1e0c0e39a3435b1dee55b888cca76
Itsupera <itsupera@gmail.com>
2021-08-29 13:50:39 +0200

    Test aeneas with AWS Polly

--- test_aeneas.sh
+++ test_aeneas.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
+# Testing aeneas ( https://github.com/readbeyond/aeneas )
 
+# Using espeak-ng for TTS => performs really badly :(
+#python3 -m aeneas.tools.execute_task \
+#    "$PWD/samples/sample1.wav" \
+#    "$PWD/samples/sample1_phonemes.txt" \
+#    "task_language=JA|os_task_file_format=aud|is_text_type=plain" -r="tts=espeak-ng|allow_unlisted_languages=True" \
+#    "$PWD/markers.txt" \
+#    --output-html
+
+# Testing with AWS Polly => working perfectly !
+# (you need to set up AWS credentials and give permissions to the user to use this service)
 python3 -m aeneas.tools.execute_task \
     "$PWD/samples/sample1.wav" \
-    "$PWD/samples/sample1_phonemes.txt" \
-    "task_language=JA|os_task_file_format=aud|is_text_type=plain" -r="tts=espeak-ng|allow_unlisted_languages=True" \
+    "$PWD/samples/sample1_kanji.txt" \
+    "task_language=Takumi|os_task_file_format=aud|is_text_type=plain" \
+    -r="allow_unlisted_languages=True|tts=aws" \
     "$PWD/markers.txt" \
-    --output-html
+    --output-html \
+    -vv
 
 firefox "$PWD/markers.txt.html"
